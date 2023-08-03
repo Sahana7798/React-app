9296375e5711da57492436fae4c4d0f5d92081d7
Sahana7798 <132565567+Sahana7798@users.noreply.github.com>
2023-08-03 09:46:25 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to Waveaxis technologies!!
+          Welcome to Waveaxis technologies..
         </p>
         <a
           className="App-link"
